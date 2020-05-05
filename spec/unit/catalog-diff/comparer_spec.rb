851e1a725a6ec8609753e87a50b26d1d4420fa65
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-05-05 16:11:24 +0200

    rubocop autofix

--- spec/unit/catalog-diff/comparer_spec.rb
+++ spec/unit/catalog-diff/comparer_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'puppet/catalog-diff/comparer'
 
 describe Puppet::CatalogDiff::Comparer do
-  include Puppet::CatalogDiff::Comparer
+  include described_class
 
   let(:res1) do
     [
@@ -15,8 +15,8 @@ describe Puppet::CatalogDiff::Comparer do
           path: '/foo',
           content: 'foo content',
           checksum: '6dbda444875c24ec1bbdb433456be11f',
-        }
-      }
+        },
+      },
     ]
   end
 
@@ -31,8 +31,8 @@ describe Puppet::CatalogDiff::Comparer do
           path: '/food',
           content: 'foo content 2',
           checksum: '4eb91aa4f5795ef3658d1e0a2798c816',
-        }
-      }
+        },
+      },
     ]
   end
 
@@ -43,7 +43,7 @@ describe Puppet::CatalogDiff::Comparer do
       },
       {
         resource_id: 'bar',
-      }
+      },
     ]
   end
 
@@ -54,42 +54,41 @@ describe Puppet::CatalogDiff::Comparer do
       },
       {
         resource_id: 'baz',
-      }
+      },
     ]
   end
 
   describe :extract_titles do
-    it 'should return resource ids' do
+    it 'returns resource ids' do
       extract_titles(resources1).should eq(['foo', 'bar'])
     end
   end
 
   describe :compare_resources do
-
-    it 'should return a diff without options' do
+    it 'returns a diff without options' do
       diffs = compare_resources(res1, res2, {})
-      expect(diffs[:old]).to eq({res1[0][:resource_id] => res1[0]})
-      expect(diffs[:new]).to eq({res2[0][:resource_id] => res2[0]})
-      expect(diffs[:old_params]).to eq({'file.foo' => {content: 'foo content', path: '/foo', checksum: '6dbda444875c24ec1bbdb433456be11f'}})
-      expect(diffs[:new_params]).to eq({'file.foo' => {content: 'foo content 2', path: '/food', checksum: '4eb91aa4f5795ef3658d1e0a2798c816'}})
+      expect(diffs[:old]).to eq(res1[0][:resource_id] => res1[0])
+      expect(diffs[:new]).to eq(res2[0][:resource_id] => res2[0])
+      expect(diffs[:old_params]).to eq('file.foo' => { content: 'foo content', path: '/foo', checksum: '6dbda444875c24ec1bbdb433456be11f' })
+      expect(diffs[:new_params]).to eq('file.foo' => { content: 'foo content 2', path: '/food', checksum: '4eb91aa4f5795ef3658d1e0a2798c816' })
       expect(diffs[:content_differences]['file.foo']).to match(%r{^\+foo content 2$})
       expect(diffs[:string_diffs]).to be_empty
     end
 
-    it 'should return string_diffs with show_resource_diff' do
-      diffs = compare_resources(res1, res2, {show_resource_diff: true})
+    it 'returns string_diffs with show_resource_diff' do
+      diffs = compare_resources(res1, res2, show_resource_diff: true)
       expect(diffs[:string_diffs]['file.foo'][3]).to eq("-\t     content => \"foo content\"")
     end
 
-    it 'should return a diff without path parameter' do
-      diffs = compare_resources(res1, res2, {ignore_parameters: 'path'})
-      expect(diffs[:old_params]).to eq({'file.foo' => {content: 'foo content', checksum: '6dbda444875c24ec1bbdb433456be11f'}})
-      expect(diffs[:new_params]).to eq({'file.foo' => {content: 'foo content 2', checksum: '4eb91aa4f5795ef3658d1e0a2798c816'}})
+    it 'returns a diff without path parameter' do
+      diffs = compare_resources(res1, res2, ignore_parameters: 'path')
+      expect(diffs[:old_params]).to eq('file.foo' => { content: 'foo content', checksum: '6dbda444875c24ec1bbdb433456be11f' })
+      expect(diffs[:new_params]).to eq('file.foo' => { content: 'foo content 2', checksum: '4eb91aa4f5795ef3658d1e0a2798c816' })
     end
   end
 
   describe :return_resource_diffs do
-    it 'should return differences' do
+    it 'returns differences' do
       diffs = return_resource_diffs(extract_titles(resources1), extract_titles(resources2))
       expect(diffs[:titles_only_in_old]).to eq(['bar'])
       expect(diffs[:titles_only_in_new]).to eq(['baz'])
@@ -97,7 +96,7 @@ describe Puppet::CatalogDiff::Comparer do
   end
 
   describe :do_str_diff do
-    it 'should diff two strings' do
+    it 'diffs two strings' do
       diff = do_str_diff('abc', 'abd')
       expect(diff).to match(/^-abc$/)
       expect(diff).to match(/^\+abd$/)
@@ -106,7 +105,7 @@ describe Puppet::CatalogDiff::Comparer do
 
   describe :str_diff do
     context 'when passing strings' do
-      it 'should diff two strings' do
+      it 'diffs two strings' do
         diff = str_diff('abc', 'abd')
         expect(diff).to match(/^-abc$/)
         expect(diff).to match(/^\+abd$/)
@@ -114,7 +113,7 @@ describe Puppet::CatalogDiff::Comparer do
     end
 
     context 'when passing hashes' do
-      it 'should diff content params' do
+      it 'diffs content params' do
         diff = str_diff(res1[0][:parameters], res2[0][:parameters])
         expect(diff).to match(/^-foo content$/)
         expect(diff).to match(/^\+foo content 2$/)
