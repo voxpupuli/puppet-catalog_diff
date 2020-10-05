f14f17ad7ce2e53ba95ad8e59853a0a708aa9d7c
John Ericson <946327+JohnEricson@users.noreply.github.com>
2020-10-05 19:02:54 +0200

    Revert "Adds backward compatibility to retrieve catalogs from older PuppetDB version 2.3 used in Puppet 3 in the latest version of puppet-catalog-diff. This is neccesary when you want to use puppet-catalog-diff to compare the cataloges between a Puppet 3 and Puppet 5 Master, as we did when we upgraded to Puppet 5. This code adds a fallback to the older /v4/nodes/?query PuppetDB API if the latest version of the API, that is /pdb/query/v4/nodes?, fails. This means that the code is compatible with both versions and thus allows the latest version of puppet-catalog-diff to still be used together with Puppet 3 servers, which is useful for those who still hasn't upgraded to newer version of Puppet."
    This reverts commit fad64487f4ca03af95c45c89b795554dbf242b2c.

--- lib/puppet/catalog-diff/searchfacts.rb
+++ lib/puppet/catalog-diff/searchfacts.rb
@@ -21,14 +21,14 @@ module Puppet::CatalogDiff
       active_nodes
     end
 
-    def build_query(env, version)
+    def find_nodes_puppetdb(env)
+      require 'puppet/util/puppetdb'
+      server_url = Puppet::Util::Puppetdb.config.server_urls[0]
+      port = server_url.port
+      use_ssl = port != 8080
+      connection = Puppet::Network::HttpPool.http_instance(server_url.host, port, use_ssl)
       base_query = ['and', ['=', ['node', 'active'], true]]
-      if version == 'latest'
-        query_field_catalog_environment = 'catalog_environment'
-      else
-        query_field_catalog_environment = 'catalog-environment'
-      end
-      base_query.concat([['=', query_field_catalog_environment, env]]) if env
+      base_query.concat([['=', 'catalog_environment', env]]) if env
       real_facts = @facts.reject { |_k, v| v.nil? }
       query = base_query.concat(real_facts.map { |k, v| ['=', ['fact', k], v] })
       classes = Hash[@facts.select { |_k, v| v.nil? }].keys
@@ -43,28 +43,9 @@ module Puppet::CatalogDiff
                ['=', 'title', capit]]]]]],
         )
       end
-      query
-    end
-
-    def find_nodes_puppetdb(env)
-      require 'puppet/util/puppetdb'
-      puppetdb_version = 'latest'
-      server_url = Puppet::Util::Puppetdb.config.server_urls[0]
-      port = server_url.port
-      use_ssl = port != 8080
-      connection = Puppet::Network::HttpPool.http_instance(server_url.host, port, use_ssl)
-      query = build_query(env, puppetdb_version)
-      json_query = URI.escape(query.to_json)
+      json_query = URI.encode_www_form_component(query.to_json)
       begin
-        result = connection.request_get("/pdb/query/v4/nodes?query=#{json_query}", 'Accept' => 'application/json')
-        if result.code.to_i >= 400
-          puppetdb_version = '2.3'
-          Puppet::debug("Query returned HTTP code #{result.code}. Falling back to older version of API used in PuppetDB version #{puppetdb_version}.")
-          query = build_query(env, puppetdb_version)
-          json_query = URI.escape(query.to_json)
-          result = connection.request_get("/v4/nodes/?query=#{json_query}", 'Accept' => 'application/json')
-        end
-        filtered = PSON.parse(result.body)
+        filtered = PSON.parse(connection.request_get("/pdb/query/v4/nodes?query=#{json_query}", 'Accept' => 'application/json').body)
       rescue PSON::ParserError => e
         raise "Error parsing json output of puppet search: #{e.message}"
       end
