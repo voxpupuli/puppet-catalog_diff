48772875db3971f4d59b60aeca5d5401d9c16eaf
dleich <daniel.leich@gmail.com>
2022-03-07 14:05:16 -0500

    create more targeted tests for viewer class

--- spec/classes/viewer_spec.rb
+++ spec/classes/viewer_spec.rb
@@ -9,6 +9,12 @@ describe 'catalog_diff::viewer' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+      it { is_expected.to contain_class('apache') }
+      it { is_expected.to contain_apache__vhost('172.16.254.254:1495') }
+      it { is_expected.to contain_htpasswd('puppet') }
+      it { is_expected.to contain_class('apache::params') }
+      it { is_expected.to contain_file('/var/www/.htpasswd') }
+      it { is_expected.to contain_vcsrepo('/var/www/diff') }
     end
   end
 end
