df0e4e97504738bf21ed68aea83064445b4e7b60
Stefan - Zipkid - Goethals <stefan@zipkid.eu>
2025-05-02 13:05:33 +0200

    Add class param to puppetdbfactory to check if the lib was loaded.

--- lib/puppet/catalog-diff/puppetdbfactory.rb
+++ lib/puppet/catalog-diff/puppetdbfactory.rb
@@ -3,16 +3,22 @@
 # Puppet::CatalogDiff
 module Puppet::CatalogDiff
   class Puppetdbfactory
+    @@puppetdb_loaded = false
     def self.puppetdb_url()
       begin
         require 'puppet/util/puppetdb'
+        @@puppetdb_loaded = true
         puppetdb_url = Puppet::Util::Puppetdb.config.server_urls[0]
       rescue LoadError
         # PuppetDB is not available, so we can't use it
         # This is fine, we can still run the catalog diff without it
-        puppetdb_url = 'PuppetDB plugin is not available! Install puppetdb-termini to enable PuppetDB functionality.'
+        puppetdb_url = 'https://puppetdb:8081'
       end
       puppetdb_url
+    end
+
+    def self.puppetdb_loaded?()
+      @@puppetdb_loaded
     end
   end
 end
