31e2ad49b029fb131d63fe9f0461f416020ac298
Stefan - Zipkid - Goethals <stefan@zipkid.eu>
2025-05-02 13:15:36 +0200

    RuboCop does NOT like class vatriables.

--- lib/puppet/catalog-diff/puppetdbfactory.rb
+++ lib/puppet/catalog-diff/puppetdbfactory.rb
@@ -3,11 +3,9 @@
 # Puppet::CatalogDiff
 module Puppet::CatalogDiff
   class Puppetdbfactory
-    @@puppetdb_loaded = false
-    def self.puppetdb_url()
+    def self.puppetdb_url
       begin
         require 'puppet/util/puppetdb'
-        @@puppetdb_loaded = true
         puppetdb_url = Puppet::Util::Puppetdb.config.server_urls[0]
       rescue LoadError
         # PuppetDB is not available, so we can't use it
@@ -15,10 +13,6 @@ module Puppet::CatalogDiff
         puppetdb_url = 'https://puppetdb:8081'
       end
       puppetdb_url
-    end
-
-    def self.puppetdb_loaded?()
-      @@puppetdb_loaded
     end
   end
 end
